244891d4e37f6cefc8fb316df19905589cd6392e
[shimojiryuki] <[23tb4008@student.gs.chiba-u.jp]>
2026-03-13 14:52:58 +0900

    ジョイスティックのロジックの修正

--- pi_mortor.py
+++ pi_mortor.py
@@ -1,11 +1,12 @@
 import RPi.GPIO as GPIO
-import time
 import zmq
 
 # ピン設定
 GPIO.setmode(GPIO.BOARD)
 MA_IN1, MA_IN2, MA_PWM = 19, 21, 23
 MB_IN1, MB_IN2, MB_PWM = 15, 13, 11
+CMD_LEFT_SIGN = 1   # 左コマンドの符号補正: 1 または -1
+CMD_RIGHT_SIGN = -1  # 右コマンドの符号補正: 1 または -1
 
 for pin in [MA_IN1, MA_IN2, MA_PWM, MB_IN1, MB_IN2, MB_PWM]:
     GPIO.setup(pin, GPIO.OUT, initial=GPIO.LOW)
@@ -15,20 +16,33 @@ pwm_b = GPIO.PWM(MB_PWM, 100)
 pwm_a.start(0)
 pwm_b.start(0)
 
-def set_motor(in1, in2, pwm, speed):
-    """速度(-100〜100)に応じてモーターの正転・逆転・停止を制御"""
+def set_left_motor(speed):
+    """左モーター速度(-100〜100)を設定"""
     if speed > 0:    # 正転
-        GPIO.output(in1, GPIO.HIGH)
-        GPIO.output(in2, GPIO.LOW)
+        GPIO.output(MB_IN1, GPIO.HIGH)
+        GPIO.output(MB_IN2, GPIO.LOW)
     elif speed < 0:  # 逆転
-        GPIO.output(in1, GPIO.LOW)
-        GPIO.output(in2, GPIO.HIGH)
+        GPIO.output(MB_IN1, GPIO.LOW)
+        GPIO.output(MB_IN2, GPIO.HIGH)
     else:            # 停止
-        GPIO.output(in1, GPIO.LOW)
-        GPIO.output(in2, GPIO.LOW)
+        GPIO.output(MB_IN1, GPIO.LOW)
+        GPIO.output(MB_IN2, GPIO.LOW)
 
-    # 速度を0〜100の範囲にしてPWMに適用
-    pwm.ChangeDutyCycle(min(abs(speed), 100))
+    pwm_b.ChangeDutyCycle(min(abs(speed), 100))
+
+def set_right_motor(speed):
+    """右モーター速度(-100〜100)を設定"""
+    if speed > 0:    # 正転
+        GPIO.output(MA_IN1, GPIO.LOW)
+        GPIO.output(MA_IN2, GPIO.HIGH)
+    elif speed < 0:  # 逆転
+        GPIO.output(MA_IN1, GPIO.HIGH)
+        GPIO.output(MA_IN2, GPIO.LOW)
+    else:            # 停止
+        GPIO.output(MA_IN1, GPIO.LOW)
+        GPIO.output(MA_IN2, GPIO.LOW)
+
+    pwm_a.ChangeDutyCycle(min(abs(speed), 100))
 
 # --- ZeroMQの設定 ---
 context = zmq.Context()
@@ -42,16 +56,19 @@ try:
         # "左の速度,右の速度" の形で送られてくるのを分割する (例: "80,40")
         try:
             l_str, r_str = cmd.split(',')
-            l_speed = int(l_str)
-            r_speed = int(r_str)
+            l_speed = int(l_str) * CMD_LEFT_SIGN
+            r_speed = int(r_str) * CMD_RIGHT_SIGN
 
-            # モーターに速度を反映 (MA=左, MB=右 と仮定しています)
-            set_motor(MA_IN1, MA_IN2, pwm_a, l_speed)
-            set_motor(MB_IN1, MB_IN2, pwm_b, r_speed)
+            set_left_motor(r_speed)
+            set_right_motor(l_speed)
         except ValueError:
             # 万が一変なデータが来たときは停止
-            set_motor(MA_IN1, MA_IN2, pwm_a, 0)
-            set_motor(MB_IN1, MB_IN2, pwm_b, 0)
+            set_left_motor(0)
+            set_right_motor(0)
 
 except KeyboardInterrupt:
     print("終了します・・・")
+finally:
+    pwm_a.stop()
+    pwm_b.stop()
+    GPIO.cleanup()
